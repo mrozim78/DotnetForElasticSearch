c7e22e2aa9cc45709e933e94d0809a29e1556de1
mrozi <mrozi.m@gmail.com>
2021-05-11 20:40:08 +0200

    Add docker-compose. Add sample app to log to elastic search.

---
+++ Elastic.Kibana.Examples/Elastic.Kibana.Examples.Serilog/Pages/Error.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Elastic.Kibana.Examples.Serilog.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            try
+            {
+                throw new Exception("Some bad code");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,"Unknown error");
+            }
+        }
+    }
+}
